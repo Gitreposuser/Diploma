0a8c78dc9d2e708a07dcbb60608c79ba5be3ae08
Gitreposuser <vlad77solo@gmail.com>
2023-06-24 15:15:47 +0200

    first commit

---
+++ src/main/java/com/example/deutschebank/DeutschebankApplication.java
@@ -0,0 +1,11 @@
+package com.example.deutschebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeutschebankApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DeutschebankApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/deutschebank/DeutschebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deutschebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeutschebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
