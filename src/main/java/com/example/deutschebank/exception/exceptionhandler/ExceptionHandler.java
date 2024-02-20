ef4afd2f4fc5b818f96c1db4573193bf966744a4
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-20 21:39:23 +0100

    Rename Handler to ExceptionHandler.

--- src/main/java/com/example/deutschebank/exception/exceptionhandler/ExceptionHandler.java
+++ src/main/java/com/example/deutschebank/exception/exceptionhandler/ExceptionHandler.java
@@ -5,18 +5,17 @@ import com.example.deutschebank.exception.BadOperationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @ControllerAdvice
-public class Handler {
-    @ExceptionHandler(BadEmailException.class)
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(BadEmailException.class)
     public ResponseEntity<String> handleBadEmailException() {
         log.error("Cannot update entity, email already exist in database!");
         return ResponseEntity.unprocessableEntity().build();
     }
 
-    @ExceptionHandler(BadOperationException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(BadOperationException.class)
     public ResponseEntity<String> handleBadOperationException() {
         log.error("Bad operation exception!");
         return ResponseEntity.unprocessableEntity().build();
