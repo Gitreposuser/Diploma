0947374ee285df8ac176af253a8c506c9294ade3
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 00:37:51 +0100

    CreateLocationDTO: add validation.

--- src/main/java/com/example/deutschebank/dto/location/CreateLocationDTO.java
+++ src/main/java/com/example/deutschebank/dto/location/CreateLocationDTO.java
@@ -1,12 +1,29 @@
 package com.example.deutschebank.dto.location;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateLocationDTO {
+    @NotNull(message = "House number should not be null!")
+    @Min(value = 1, message = "House number should be greater than 0!")
     private Integer houseNumber;
+
+    @NotNull(message = "Street name should not be null!")
+    @NotBlank(message = "Street name should not be empty!")
     private String street;
+
+    @NotNull(message = "City name should not be null!")
+    @NotBlank(message = "City name should not be empty!")
     private String city;
+
+    @NotNull(message = "State should not be null!")
+    @NotBlank(message = "State should not be empty!")
     private String state;
+
+    @NotNull(message = "Country name should not be null!")
+    @NotBlank(message = "Country name should not be empty!")
     private String country;
 }
