4726d5ff6d9db74e680c5048e4c84940a15d934d
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 00:41:04 +0100

    UpdateLocationDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/location/UpdateLocationDTO.java
+++ src/main/java/com/example/deutschebank/dto/location/UpdateLocationDTO.java
@@ -1,15 +1,34 @@
 package com.example.deutschebank.dto.location;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class UpdateLocationDTO {
+    @NotNull(message = "Location id should not be null!")
     private UUID id;
+
+    @NotNull(message = "House number should not be null!")
+    @Min(value = 1, message = "House number should be greater than 0!")
     private Integer houseNumber;
+
+    @NotNull(message = "Street name should not be null!")
+    @NotBlank(message = "Street name should not be empty!")
     private String street;
+
+    @NotNull(message = "City name should not be null!")
+    @NotBlank(message = "City name should not be empty!")
     private String city;
+
+    @NotNull(message = "State should not be null!")
+    @NotBlank(message = "State should not be empty!")
     private String state;
+
+    @NotNull(message = "Country name should not be null!")
+    @NotBlank(message = "Country name should not be empty!")
     private String country;
 }
