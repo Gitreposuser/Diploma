3fc5d22c3b03c31b5f108248e79953a66408aed4
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 21:38:37 +0100

    UpdateClientDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/client/UpdateClientDTO.java
+++ src/main/java/com/example/deutschebank/dto/client/UpdateClientDTO.java
@@ -4,15 +4,27 @@ import com.example.deutschebank.entity.DebitAccount;
 import com.example.deutschebank.entity.Employee;
 import com.example.deutschebank.entity.Location;
 import com.example.deutschebank.entity.PersonalDetail;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.util.UUID;
 
 @Data
 public class UpdateClientDTO {
+    @NotNull(message = "Id should not be null!")
     private UUID id;
+
+    @NotNull(message = "Personal detail should not be null!")
     private PersonalDetail personalDetail;
+
+    @NotNull(message = "Debit account detail should not be null!")
     private DebitAccount debitAccount;
+
+    @NotNull(message = "Location should not be null!")
     private Location location;
+
+    @NotNull(message = "Employee should not be null!")
     private Employee employee;
+
+    @NotNull(message = "Active should not be null!")
     private Boolean active;
 }
