6b00149bd97bed87db2bba84c2798b1510dc8458
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 01:28:09 +0100

    CreateTransactionDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/transaction/CreateTransactionDTO.java
+++ src/main/java/com/example/deutschebank/dto/transaction/CreateTransactionDTO.java
@@ -1,12 +1,24 @@
 package com.example.deutschebank.dto.transaction;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class CreateTransactionDTO {
+
+    @NotNull(message = "Emitter Iban should not be null!")
+    @NotBlank(message = "Emitter Iban should not be empty!")
     private String emitterIban;
+
+    @NotNull(message = "Receiver Iban should not be null!")
+    @NotBlank(message = "Receiver Iban should not be empty!")
     private String receiverIban;
+
+    @NotNull(message = "Amount should not be null!")
+    @Min(value = 1, message = "Amount should start from 1!")
     private BigDecimal amount;
 }
