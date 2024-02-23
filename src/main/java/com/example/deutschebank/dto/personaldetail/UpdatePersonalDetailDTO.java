617239c0befaaf026a0ded7db9d57011aed178d2
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 01:18:04 +0100

    UpdatePersonalDetailDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/personaldetail/UpdatePersonalDetailDTO.java
+++ src/main/java/com/example/deutschebank/dto/personaldetail/UpdatePersonalDetailDTO.java
@@ -1,6 +1,7 @@
 package com.example.deutschebank.dto.personaldetail;
 
 import com.example.deutschebank.entity.enums.Sex;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,14 +9,43 @@ import java.util.UUID;
 
 @Data
 public class UpdatePersonalDetailDTO {
+    @NotNull(message = "First name should not be null!")
     private UUID id;
+
+    @NotNull(message = "First name should not be null!")
+    @NotBlank(message = "First name should not be empty!")
     private String firstName;
+
+    @NotNull(message = "Last name should not be null!")
+    @NotBlank(message = "Last name should not be empty!")
     private String lastName;
+
+    @NotNull(message = "Sex should not be null!")
     private Sex sex;
+
+    @NotBlank(message = "Phone number should not be empty!")
+    @NotNull(message = "Phone number should not be null!")
+    @Size(min = 9, max = 18, message = "Phone number should be in range " +
+            "from 9 to 18 digits!")
+    @Pattern(regexp = "^(?:[0-9]+[-. ()]*)+$",
+            message = "Invalid hot line number!")
     private String phone;
+
+    @Email(message = "Wrong email format!")
     private String email;
+
+    @NotNull(message = "Age should not be null!")
+    @Min(value = 12, message = "Age should be greater than 12!")
     private Integer age;
+
+    @NotNull(message = "Birth date should not be null!")
+    @Past(message = "Birth date must be in the past!")
     private LocalDateTime birthDate;
+
+    @NotNull(message = "IsMarried should not be null!")
     private Boolean isMarried;
+
+    @NotNull(message = "Children number should not be null!")
+    @Min(value = 0, message = "Children number should be greater or equal to 0!")
     private Integer children;
 }
