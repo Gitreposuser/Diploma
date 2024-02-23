4075aa944dfb95122069e27b251762adee2b67a7
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 21:52:13 +0100

    CreateCreditAccountDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/creditaccount/CreateCreditAccountDTO.java
+++ src/main/java/com/example/deutschebank/dto/creditaccount/CreateCreditAccountDTO.java
@@ -1,16 +1,34 @@
 package com.example.deutschebank.dto.creditaccount;
 
 import com.example.deutschebank.entity.Client;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class CreateCreditAccountDTO {
+    @NotNull(message = "Client should not be null!")
     private Client client;
+
+    @NotNull(message = "Reason should not be null!")
+    @NotBlank(message = "Reason should not be empty!")
     private String reason;
+
+    @NotNull(message = "Credit status should not be null!")
+    @NotBlank(message = "Credit status should not be empty!")
     private String creditStatus;
+
+    @NotNull(message = "Debt should not be null!")
+    @Min(value = 1, message = "Debt should start from 1!")
     private BigDecimal debt;
+
+    @NotNull(message = "Loan interest should not be null!")
+    @Min(value = 1, message = "Loan interest should start from 1!")
     private BigDecimal loanInterest;
+
+    @NotNull(message = "Active should not be null!")
     private Boolean active;
 }
