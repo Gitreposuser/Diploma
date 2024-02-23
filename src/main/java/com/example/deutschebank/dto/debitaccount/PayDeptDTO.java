561b64b49c6a9c323d7ddfa2634a4dced86b6adf
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 22:00:01 +0100

    PayDeptDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/debitaccount/PayDeptDTO.java
+++ src/main/java/com/example/deutschebank/dto/debitaccount/PayDeptDTO.java
@@ -1,5 +1,7 @@
 package com.example.deutschebank.dto.debitaccount;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,7 +9,13 @@ import java.util.UUID;
 
 @Data
 public class PayDeptDTO {
+    @NotNull(message = "UUID should not null!")
     private UUID debitAccountId;
+
+    @NotNull(message = "UUID should not null!")
     private UUID creditAccountId;
+
+    @NotNull(message = "Amount should not be null!")
+    @Min(value = 1, message = "Amount should start from 1!")
     private BigDecimal amount;
 }
