c136465b9adee1de2f3ee4b78ffe9448c2c0ecc5
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 22:10:38 +0100

    TransferFundsDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/debitaccount/TransferFundsDTO.java
+++ src/main/java/com/example/deutschebank/dto/debitaccount/TransferFundsDTO.java
@@ -1,12 +1,23 @@
 package com.example.deutschebank.dto.debitaccount;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class TransferFundsDTO {
+    @NotNull(message = "Amount should not be null!")
+    @Min(value = 1, message = "Amount should start from 1!")
     private BigDecimal amount;
+
+    @NotNull(message = "Emitter iban should not be null!")
+    @NotBlank(message = "Emitter iban should not be empty!")
     private String emitterIban;
+
+    @NotNull(message = "Receiver iban should not be null!")
+    @NotBlank(message = "Receiver iban should not be empty!")
     private String receiverIban;
 }
