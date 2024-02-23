572627ed6cc0ba08515a1c6b6085c502bd7a6e2f
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 21:36:00 +0100

    CreateClientDto: add fields validation.

--- src/main/java/com/example/deutschebank/dto/client/CreateClientDTO.java
+++ src/main/java/com/example/deutschebank/dto/client/CreateClientDTO.java
@@ -4,13 +4,23 @@ import com.example.deutschebank.entity.DebitAccount;
 import com.example.deutschebank.entity.Employee;
 import com.example.deutschebank.entity.Location;
 import com.example.deutschebank.entity.PersonalDetail;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateClientDTO {
+    @NotNull(message = "Personal detail should not be null!")
     private PersonalDetail personalDetail;
+
+    @NotNull(message = "Debit account detail should not be null!")
     private DebitAccount debitAccount;
+
+    @NotNull(message = "Location should not be null!")
     private Location location;
+
+    @NotNull(message = "Employee should not be null!")
     private Employee employee;
+
+    @NotNull(message = "Active should not be null!")
     private Boolean active;
 }
