ad5e8004cfe27be9bea21c0e57d19e343a215df2
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 22:01:54 +0100

    CreateEmployeeDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/employee/CreateEmployeeDTO.java
+++ src/main/java/com/example/deutschebank/dto/employee/CreateEmployeeDTO.java
@@ -4,13 +4,23 @@ import com.example.deutschebank.entity.BankBranch;
 import com.example.deutschebank.entity.Location;
 import com.example.deutschebank.entity.PersonalDetail;
 import com.example.deutschebank.entity.WorkDetail;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateEmployeeDTO {
+    @NotNull(message = "Personal detail should not be null!")
     private PersonalDetail personalDetail;
+
+    @NotNull(message = "Work detail should not be null!")
     private WorkDetail workDetail;
+
+    @NotNull(message = "Location should not be null!")
     private Location location;
+
+    @NotNull(message = "Bank branch should not be null!")
     private BankBranch bankBranch;
+
+    @NotNull(message = "Active should not be null!")
     private Boolean active;
 }
