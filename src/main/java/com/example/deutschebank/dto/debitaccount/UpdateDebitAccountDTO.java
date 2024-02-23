a235d9358d3db2354c1ed35ee4ba9e680a0a97ec
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-23 21:58:45 +0100

    UpdateDebitAccountDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/debitaccount/UpdateDebitAccountDTO.java
+++ src/main/java/com/example/deutschebank/dto/debitaccount/UpdateDebitAccountDTO.java
@@ -1,5 +1,8 @@
 package com.example.deutschebank.dto.debitaccount;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,10 +10,26 @@ import java.util.UUID;
 
 @Data
 public class UpdateDebitAccountDTO {
+    @NotNull(message = "Id should not be null!")
     private UUID id;
+
+    @NotNull(message = "Iban should not be null!")
+    @NotBlank(message = "Iban should not be empty!")
     private String iban;
+
+    @NotNull(message = "Debit status should not be null!")
+    @NotBlank(message = "Debit status should not be empty!")
     private String debitStatus;
+
+    @NotNull(message = "Balance should not be null!")
+    @Min(value = 0, message = "Balance should start from 0!")
     private BigDecimal balance;
+
+    @NotNull(message = "Deposit interest should not be null!")
+    @Min(value = 0, message = "Deposit interest should start from 0!")
     private BigDecimal depositInterest;
+
+    @NotNull(message = "Credit line should not be null!")
+    @Min(value = 0, message = "Credit line should start from 0!")
     private BigDecimal creditLine;
 }
