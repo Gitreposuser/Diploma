523dbf0fc272e3723f976ce313cca9b30f7be7a3
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-24 18:35:30 +0100

    EmailDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/additional/email/EmailDTO.java
+++ src/main/java/com/example/deutschebank/dto/additional/email/EmailDTO.java
@@ -1,12 +1,21 @@
 package com.example.deutschebank.dto.additional.email;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class EmailDTO {
+    @NotNull(message = "Email to could not be null!")
     private String[] emailTo;
     private String[] cc;
     private String[] bcc;
+
+    @NotNull(message = "Subject could not be null!")
+    @NotBlank(message = "Subject could not be empty!")
     private String subject;
+
+    @NotNull(message = "Text could not be null!")
+    @NotBlank(message = "Text could not be empty!")
     private String text;
 }
