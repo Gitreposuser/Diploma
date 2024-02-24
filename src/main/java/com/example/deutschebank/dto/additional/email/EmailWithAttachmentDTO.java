2c5c56ced734850179f6730dbd07f1195d7ffce8
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-02-24 18:58:30 +0100

    EmailWithAttachmentDTO: add fields validation.

--- src/main/java/com/example/deutschebank/dto/additional/email/EmailWithAttachmentDTO.java
+++ src/main/java/com/example/deutschebank/dto/additional/email/EmailWithAttachmentDTO.java
@@ -1,13 +1,25 @@
 package com.example.deutschebank.dto.additional.email;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class EmailWithAttachmentDTO {
+    @NotNull(message = "Email to could not be null!")
     private String[] emailTo;
     private String[] cc;
     private String[] bcc;
+
+    @NotNull(message = "Subject could not be null!")
+    @NotBlank(message = "Subject could not be empty!")
     private String subject;
+
+    @NotNull(message = "Text could not be null!")
+    @NotBlank(message = "Text could not be empty!")
     private String text;
+
+    @NotNull(message = "Path to attachment could not be null!")
+    @NotBlank(message = "Path to attachment could not be empty!")
     private String pathToAttachment;
 }
